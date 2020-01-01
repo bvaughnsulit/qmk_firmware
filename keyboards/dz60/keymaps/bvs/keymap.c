d21e47b3106752a555c12fb313b4c05c7acca38d
bvaughnsulit <bvaughnsulit@gmail.com>
2019-12-31 18:31:24 -0800

    reset boardwalk, fix del in dz60

--- keyboards/dz60/keymaps/bvs/keymap.c
+++ keyboards/dz60/keymaps/bvs/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_SPC, KC_RGUI, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT),
 
 	[L1] = LAYOUT(
-		KC_GRAVE, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, _______,
+		KC_GRAVE, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, KC_DEL,
 		RGB_TOG, _______, _______, _______, _______, _______, _______, _______, KC_UP, _______, _______, _______, _______, KC_DEL,
 		_______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, KC_GRAVE, _______,
 		_______, _______, _______, _______, _______, _______, MO(L2), _______, _______, _______, _______, _______, KC__MUTE, _______,
